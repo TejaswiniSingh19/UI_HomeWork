e7e206ae5421a5c3342f84ba43933bcf4f940226
shalu-PC\shalu <tejaswinisingh0@gmail.com>
2016-10-29 18:17:44 -0400

    commit

--- UI_HomeWork/JavaScript/eg2.js
+++ UI_HomeWork/JavaScript/eg2.js
@@ -9,4 +9,6 @@
 
 			var book1 = new book("Chethan Bhagath", "Rose", 45);
 	
-document.write(book1.age)
+            document.write(book1.age+"<br/>")
+            document.write(book1.publish+"<br>")
+            document.write(book1.author)
