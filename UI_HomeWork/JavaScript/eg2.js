6a319a35129ecfebe962108935aab830e633b583
shalu-PC\shalu <tejaswinisingh0@gmail.com>
2016-10-29 13:48:43 -0400

    commit

---
+++ UI_HomeWork/JavaScript/eg2.js
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+	<body>
+		<script>
+			function book(author, publisher, age, releasedate) {
+    		this.bauthor  = author;
+    		this.bpublish = publisher;
+    		this.bage     = age;
+   		    this.breldate = releasedate;
+			}
+
+
+			var revolution = new book("Chethan Bhagath", "Rose", 45, );
+			var  = new book("Sally", "Rally", 48, "green");
+
+			document.getElementById("demo").innerHTML =
+			"My father is " + myFather.age + ". My mother is " + myMother.age;
+		</script>
+
+	</body>
+</html>
